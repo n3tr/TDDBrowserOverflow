015abf899c7dd5767faec541eb8052c7efd48af2
Jirat K <saakyz@gmail.com>
2014-01-06 14:28:16 +0700

    Implement Person model
    - Person: name, avatarURL.
    - Tests.

---
+++ TDDBrowserOverflowTests/Tests/Models/PersonTests.m
@@ -0,0 +1,46 @@
+//
+//  PeopleTests.m
+//  TDDBrowserOverflow
+//
+//  Created by Jirat K. on 1/6/2557 BE.
+//  Copyright (c) 2557 Jirat K. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Person.h"
+@interface PersonTests : XCTestCase
+
+@property (nonatomic, strong) Person *person;
+
+@end
+
+@implementation PersonTests
+
+- (void)setUp
+{
+    [super setUp];
+    _person = [[Person alloc] initWithName:@"Jirat K." avatarLocation:@"http://example.com/avatar.png"];
+}
+
+- (void)tearDown
+{
+    _person = nil;
+    [super tearDown];
+}
+
+- (void)testThatPersonHasTheRightName
+{
+    XCTAssertEqualObjects(_person.name, @"Jirat K.", @"expecting a person to provide its name");
+}
+
+
+- (void)testThatPersonHasAnAvatarURL
+{
+    XCTAssertTrue([_person.avatarURL isKindOfClass:[NSURL class]], @"Expecting avatar url is instance of NSURL");
+    
+    NSURL *url = _person.avatarURL;
+    XCTAssertEqualObjects([url absoluteString], @"http://example.com/avatar.png", @"The Person's avatar should be represented by a URL");
+}
+
+
+@end
